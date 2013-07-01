adc8bb81f5a2f063787b16eccf25d24079853fbf
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-01 05:42:18 -0700

    working example for partially giving options

---
+++ examples/username-password-one-given.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var promfig = require('..')
+var properties = { 
+    user      :  'Please enter your username :  '
+  , password  :  'Please enter your password :  '
+  , '@secret' :  'password'
+  };
+var config = { user: 'humptydumpty' };
+
+promfig(
+    properties
+  , config 
+  , function (err, config) {
+      if (err) return console.error('err: ', err);
+      console.log(config);
+  }
+);
